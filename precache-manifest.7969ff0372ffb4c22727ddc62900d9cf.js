5f855a19e5c327bf18483a5101571b3998206642
ligsh <system6@jkchemical.com>
2019-10-10 11:48:29 +0800

    Updates

---
+++ precache-manifest.7969ff0372ffb4c22727ddc62900d9cf.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b531d09aa13406f0014e2078ded1231",
+    "url": "./index.html"
+  },
+  {
+    "revision": "83b2dc68a4694e6eb1a3",
+    "url": "./static/css/2.d2218183.chunk.css"
+  },
+  {
+    "revision": "e26dfcb275d9987441ed",
+    "url": "./static/css/main.3bf014a0.chunk.css"
+  },
+  {
+    "revision": "83b2dc68a4694e6eb1a3",
+    "url": "./static/js/2.1b24e047.chunk.js"
+  },
+  {
+    "revision": "50152bd9082eef612780",
+    "url": "./static/js/3.b8ecc287.chunk.js"
+  },
+  {
+    "revision": "d595fc0d72a7a732f8f2",
+    "url": "./static/js/4.255d5f95.chunk.js"
+  },
+  {
+    "revision": "e26dfcb275d9987441ed",
+    "url": "./static/js/main.9e3f751b.chunk.js"
+  },
+  {
+    "revision": "08b4d5d00743f0faeee0",
+    "url": "./static/js/runtime-main.26de484f.js"
+  },
+  {
+    "revision": "f85ff3e0a81d38e50a00c59f0687c339",
+    "url": "./static/media/AnalyticalChemistry.f85ff3e0.png"
+  },
+  {
+    "revision": "3f6ce00f7bed58ef38b348940f7810ea",
+    "url": "./static/media/LabSupplies.3f6ce00f.png"
+  },
+  {
+    "revision": "5c2e6ff5bf4fefbcca588b64884c53e3",
+    "url": "./static/media/LifeScience.5c2e6ff5.png"
+  },
+  {
+    "revision": "f2439aa0014b9b2ac73689d5034c9934",
+    "url": "./static/media/MaterialScience.f2439aa0.png"
+  },
+  {
+    "revision": "6d56971c2c7ae9eb3b49ec6630da17b8",
+    "url": "./static/media/OrganicChemistry.6d56971c.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "772a31a646cb82c2cf0bdebade5fee51",
+    "url": "./static/media/pig.772a31a6.png"
+  }
+]);
