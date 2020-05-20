369f5f2b15eaefdff19d06171bc5b5a6490d3ce7
ligsh <system6@jkchemical.com>
2020-05-20 17:23:40 +0800

    ligsh,ver1.1.86,修复积分商城中产品列表不显示的问题

--- src/configuration.ts
+++ src/configuration.ts
@@ -7,7 +7,7 @@ export { CApp } from './CApp';
 
 export const appConfig: AppConfig = {
     appName: '百灵威系统工程部/cart',
-    version: '1.1.85',
+    version: '1.1.86',
     tvs: tvs,
     loginTop: jnkTop,
     oem: '百灵威',
--- src/pointMarket/VPointProduct.tsx
+++ src/pointMarket/VPointProduct.tsx
@@ -88,17 +88,7 @@ export class VPointProduct extends VPage<CPointProduct> {
     }
 
     private page = observer(() => {
-        let { pointProducts } = this.controller;
-        let footer = <this.ExchangePoint />;
-        return <Page header="积分商城" footer={footer} right={null}>
-            <div>
-                <List items={pointProducts} item={{ render: this.renderPointProduct }} none="暂无可兑换产品"></List>
-            </div>
-        </Page >;
-    });
-
-    protected ExchangePoint = observer(() => {
-        let { pointToExchanging, myEffectivePoints } = this.controller;
+        let { pointProducts, pointToExchanging, myEffectivePoints } = this.controller;
 
         let productIsNullTip = this.productIsNull ?
             <div className="text-danger small my-2"><FA name="exclamation-circle" />未选择产品</div>
@@ -107,12 +97,17 @@ export class VPointProduct extends VPage<CPointProduct> {
             <div className="text-danger small m-0 p-0"><FA name="exclamation-circle" />积分不足<br />剩余{myEffectivePoints}分</div>
             : null;
 
-        return <div className="d-block">
+        let footer = <div className="d-block">
             <div className="w-100 px-3 d-flex justify-content-between">
                 <div>总计:<span className="text-danger ml-2 mr-1 h2" >{pointToExchanging}</span>分</div>
                 <div>{productIsNullTip}{pointIsEnoughTip}</div>
                 <button type="button" className="btn btn-danger m-1" onClick={this.openExchangeOrder}>去兑换</button>
             </div>
         </div>;
+        return <Page header="积分商城" footer={footer}>
+            <div>
+                <List items={pointProducts} item={{ render: this.renderPointProduct }} none="暂无可兑换产品"></List>
+            </div>
+        </Page>;
     });
 }
