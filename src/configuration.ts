16ae45769d07fcb26bb03063fcabe388a0c98694
ligsh <system6@jkchemical.com>
2020-07-24 19:08:43 +0800

    ligsh, ver1.1.107，恢复在android上提示下载app的功能

--- src/configuration.ts
+++ src/configuration.ts
@@ -8,7 +8,7 @@ export { CApp } from './CApp';
 
 export const appConfig: AppConfig = {
     appName: '百灵威系统工程部/cart',
-    version: '1.1.106',
+    version: '1.1.107',
     tvs: tvs,
     loginTop: jnkTop,
     oem: '百灵威',
