638a5fa23445e776dce81b5634081502834ef4da
ligsh <system6@jkchemical.com>
2020-08-04 16:45:12 +0800

    ligsh, ver1.1.113,暂时屏蔽卡券管理界面的滚动分页功能(会有错误)

--- src/configuration.ts
+++ src/configuration.ts
@@ -7,7 +7,7 @@ export { CApp } from './CApp';
 
 export const appConfig: AppConfig = {
     appName: '百灵威系统工程部/cart',
-    version: '1.1.112',
+    version: '1.1.113',
     tvs: tvs,
     loginTop: jnkTop,
     oem: '百灵威',
