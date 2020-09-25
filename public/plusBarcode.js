618b5b92a668f1b012987caecdbd86c34b6a6236
ZWGKing <nage@163.com>
2020-09-25 18:19:05 +0800

    扫一扫调整

--- public/plusBarcode.js
+++ public/plusBarcode.js
@@ -1,73 +1,59 @@
 /* eslint-disable default-case */
 /* eslint-disable no-undef */
 (function () {
-
-    var plusReady = function (callback) {
-        if (window.plus) {
-            callback();
-        } else {
-            document.addEventListener('plusready', callback);
-        }
-    }
-
-    /* --------- */
-    // 扩展API加载完毕后调用onPlusReady回调函数 
-    // document.addEventListener("plusready", onPlusReady, false);
-    // 扩展API加载完毕，现在可以正常调用扩展API
-    // 扩展API加载完毕后调用onPlusReady回调函数 
-    document.addEventListener("plusready", onPlusReady, false);
-    // 扩展API加载完毕，现在可以正常调用扩展API
-    function onPlusReady() {
-        var e = document.getElementById("scan");
-        e.removeAttribute("disabled");
-    }
+    var ws = null, wo = null;
     var scan = null;
-    function onmarked(type, result) {
-        var text = '未知: ';
+    // H5 plus事件处理
+    function plusReady() {
+        // 获取窗口对象
+        ws = plus.webview.currentWebview();
+        wo = ws.opener();
+        // 开始扫描
+        ws.addEventListener('show', function () {
+            scan = new plus.barcode.Barcode('bcid');
+            scan.onmarked = onmarked;
+            scan.start({ conserve: true, filename: '_doc/barcode/' });
+            createSubview();
+        }, false);
+        // 显示页面并关闭等待框
+        ws.show('pop-in');
+    }
+    document.addEventListener('plusready', plusReady, false);
+
+    // 二维码扫描成功
+    function onmarked(type, result, file) {
         switch (type) {
             case plus.barcode.QR:
-                text = 'QR: ';
+                type = 'QR';
                 break;
             case plus.barcode.EAN13:
-                text = 'EAN13: ';
+                type = 'EAN13';
                 break;
             case plus.barcode.EAN8:
-                text = 'EAN8: ';
+                type = 'EAN8';
+                break;
+            default:
+                type = '其它' + type;
                 break;
         }
-        result = result.replace(/\n/g, '');
-        console.log('result---', result);
-        if (result.indexOf('http://') == 0 || result.indexOf('https://') == 0) {
-            plus.nativeUI.confirm(result, function (i) {
-                if (i.index == 0) {
-                    // self.back() // 返回上个页面
-
-                    plus.runtime.openURL(result)
-                    console.log(result) // 扫出来的值
-                    t.scan.close() //扫码成功后关闭扫码
-                    t.$router.push({ path: 'Info', query: { id: result } })   // 跳转到对应的页面 
-                } else {
-                    // self.back() // 返回上个页面
-                    console.log(result)
-                    t.scan.close()
-                    window.localStorage.scan = result
-                    t.$router.push({ path: 'Info', query: { id: result } })
-                }
-            }, '', ['打开', '取消'])
-        } else {
-            // self.back() // 返回上个页面
-            console.log(result)
-        }
+        result = result.replace(/\r\n/g, '');
+        wo.evalJS("scaned('" + type + "','" + result + "','" + file + "');");
+        back();
     }
-    function startRecognize() {
-        scan = new plus.barcode.Barcode('bcid');
-        startScan();
-        scan.onmarked = onmarked;
+    // 创建子窗口
+    function createSubview() {
+        //
     }
-    function startScan() {
-        scan.start();
-        console.log('start');
+    // 从相册中选择二维码图片 
+    function scanPicture() {
+        plus.gallery.pick(function (path) {
+            plus.barcode.scan(path, onmarked, function (error) {
+                plus.nativeUI.alert('无法识别此图片');
+            });
+        }, function (err) {
+            console.log('Failed: ' + err.message);
+        });
     }
 
-    window.plusBarcode = startRecognize// open;
+    window.plusBarcode = plusReady
 })();
