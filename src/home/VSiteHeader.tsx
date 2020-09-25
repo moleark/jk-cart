815470199b64e4868f1c01f31cac7505539c00a6
ZWGKing <nage@163.com>
2020-09-25 17:25:19 +0800

    扫一扫调整

--- src/home/VSiteHeader.tsx
+++ src/home/VSiteHeader.tsx
@@ -8,8 +8,8 @@ export class VSiteHeader extends View<CHome> {
         let currentSalesRegion = <FA name="globe" />
         let left = <img className="m-1 ml-2" src={logo} alt="logo" style={{ height: "3rem", width: "2.5rem" }} />;
         //let cart = this.controller.cApp.cCart.renderCartLabel();
-        // let right = undefined; // d-block d-md-none
-        let right = <img src={扫一扫} alt="" className="mr-2" style={{ width: 24 }} onClick={this.controller.openScanCode} />;
+        let right = undefined; // d-block d-md-none
+        // let right = <img src={扫一扫} alt="" className="mr-2" style={{ width: 24 }} onClick={this.controller.openScanCode} />;
         /*
         <div className="d-flex flex-row mr-1 align-items-center">
             {currentSalesRegion} &nbsp;
@@ -20,6 +20,7 @@ export class VSiteHeader extends View<CHome> {
             left={left} right={right}>
             <div className="px-3 py-4">
                 {this.controller.renderSearchHeader('md')}
+                <img src={扫一扫} alt="" className="mr-2 mt-2" style={{ width: 30 }} onClick={this.controller.openScanCode} />
             </div>
         </LMR>
         //<div className="h4 px-3 mb-0">百灵威科技</div>
