ccbff051cf7a59e139d09ec0129aa802fbb70bd5
ZWGKing <nage@163.com>
2020-09-27 10:41:56 +0800

    扫描test

--- src/home/VScanCode.tsx
+++ src/home/VScanCode.tsx
@@ -37,13 +37,13 @@ export class VScanCode extends VPage<CHome> {
     }
 
     private page = () => {
-        this.media();
+        // this.media();
         return <div className="w-100 h-100 position-relative">
-            <div className="position-absolute w-100" style={{ zIndex: 9 }}>
+            {/*  <div className="position-absolute w-100" style={{ zIndex: 9 }}>
                 {CustomHeaderTemplate('扫码', '', 'bg-transparent', 'text-primary')}
-            </div>
+            </div> */}
+            <button style={{ zIndex: 99 }} onClick={this.media} >创建</button>
             <div id='bcid' className="w-100 h-100 bg-dark">
-                <div className="w-50"></div>
             </div>
         </div>;
     };
