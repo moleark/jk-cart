0fd7625d8f74545ab5df34e7b8cdafae40aaa510
ZWGKing <nage@163.com>
2020-12-01 18:12:36 +0800

    zwg,Ver,1.1.125,U_Fix;1.积分商品兑换积分总计bug

--- src/configuration.ts
+++ src/configuration.ts
@@ -7,7 +7,7 @@ export { CApp } from './CApp';
 
 export const appConfig: AppConfig = {
     appName: '百灵威系统工程部/cart',
-    version: '1.1.124',
+    version: '1.1.125',
     tvs: tvs,
     loginTop: jnkTop,
     oem: '百灵威',
