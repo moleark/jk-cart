c6dd4799359ccde501199e52370cff91b3b1206d
ligsh <system6@jkchemical.com>
2020-12-24 12:06:58 +0800

    ligsh, ver 1.1.131, 启用service worker, 解决app版本不能及时更新的问题

--- src/configuration.ts
+++ src/configuration.ts
@@ -7,7 +7,7 @@ export { CApp } from './CApp';
 
 export const appConfig: AppConfig = {
     appName: '百灵威系统工程部/cart',
-    version: '1.1.130',
+    version: '1.1.131',
     tvs: tvs,
     loginTop: jnkTop,
     oem: '百灵威',
