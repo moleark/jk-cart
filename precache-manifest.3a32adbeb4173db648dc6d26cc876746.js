a61baa8c35629228cfbd39159f0bfd59d316c753
ligsh <system6@jkchemical.com>
2021-02-09 18:13:09 +0800

    Updates

---
+++ precache-manifest.3a32adbeb4173db648dc6d26cc876746.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4ed16b6dedd715af5d3a57772ac1e5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b3cc074d603e620acbd4",
+    "url": "./static/css/2.e226fe3a.chunk.css"
+  },
+  {
+    "revision": "97e7fe3ea961e7babd55",
+    "url": "./static/css/main.8728c00d.chunk.css"
+  },
+  {
+    "revision": "b3cc074d603e620acbd4",
+    "url": "./static/js/2.eaa822f5.chunk.js"
+  },
+  {
+    "revision": "6dbf46da41846f186910887b5c3ec3a2",
+    "url": "./static/js/2.eaa822f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97e7fe3ea961e7babd55",
+    "url": "./static/js/main.87a36182.chunk.js"
+  },
+  {
+    "revision": "f3bf36e81ba3e4da0e32",
+    "url": "./static/js/runtime-main.605d384a.js"
+  },
+  {
+    "revision": "f85ff3e0a81d38e50a00c59f0687c339",
+    "url": "./static/media/AnalyticalChemistry.f85ff3e0.png"
+  },
+  {
+    "revision": "06f15feb8324ae70938e87c9b09032f6",
+    "url": "./static/media/Homemadelogo.06f15feb.png"
+  },
+  {
+    "revision": "3f6ce00f7bed58ef38b348940f7810ea",
+    "url": "./static/media/LabSupplies.3f6ce00f.png"
+  },
+  {
+    "revision": "5c2e6ff5bf4fefbcca588b64884c53e3",
+    "url": "./static/media/LifeScience.5c2e6ff5.png"
+  },
+  {
+    "revision": "f2439aa0014b9b2ac73689d5034c9934",
+    "url": "./static/media/MaterialScience.f2439aa0.png"
+  },
+  {
+    "revision": "86a66d1b4fdfdcf1ee68d7fe2d398ae6",
+    "url": "./static/media/Menu.86a66d1b.svg"
+  },
+  {
+    "revision": "6d56971c2c7ae9eb3b49ec6630da17b8",
+    "url": "./static/media/OrganicChemistry.6d56971c.png"
+  },
+  {
+    "revision": "abe107d7081b4199afcab43a00f67a4d",
+    "url": "./static/media/TriangleShadingO.abe107d7.png"
+  },
+  {
+    "revision": "7a55757eea6c2a794ae9e83f9aaca29d",
+    "url": "./static/media/TriangleShadingT.7a55757e.png"
+  },
+  {
+    "revision": "c130720600192b97f2582c6d2e54ffbe",
+    "url": "./static/media/cards.c1307206.png"
+  },
+  {
+    "revision": "b3f6f904efe5eaa7eab08ca32e4c73fc",
+    "url": "./static/media/chevron-right-hover.b3f6f904.svg"
+  },
+  {
+    "revision": "d59775bb19969d7446b23c8562e51784",
+    "url": "./static/media/commerce-and-shopping-hover.d59775bb.svg"
+  },
+  {
+    "revision": "0889c62634c30c1dd85896be9daa5312",
+    "url": "./static/media/commerce-and-shopping.0889c626.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "2c097c9eaa3df6c406104e770b316d9a",
+    "url": "./static/media/footer-bg.2c097c9e.svg"
+  },
+  {
+    "revision": "015bdb05607e99a055a2ee8d342a3d17",
+    "url": "./static/media/giftPlate.015bdb05.png"
+  },
+  {
+    "revision": "89cdf192cc2a11fbe8e040d00be8eb0a",
+    "url": "./static/media/homeTopicMap.89cdf192.jpg"
+  },
+  {
+    "revision": "ce9e0db2426657f1e934e85f59aec03f",
+    "url": "./static/media/infb.ce9e0db2.jpg"
+  },
+  {
+    "revision": "03c9d58e751d9355a0dcff6ab039601d",
+    "url": "./static/media/left-arrow.03c9d58e.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "644af5a600da30d4813730f9f3249e86",
+    "url": "./static/media/logo_pointShop.644af5a6.png"
+  },
+  {
+    "revision": "d3e87675886d3eb7031aee056dad1ddd",
+    "url": "./static/media/number-01.d3e87675.svg"
+  },
+  {
+    "revision": "ebc126798e670e1f3edffcd4f9bf7f9e",
+    "url": "./static/media/order-01.ebc12679.svg"
+  },
+  {
+    "revision": "772a31a646cb82c2cf0bdebade5fee51",
+    "url": "./static/media/pig.772a31a6.png"
+  },
+  {
+    "revision": "572250564939cbb2cff1401d8b524779",
+    "url": "./static/media/s-triangle.57225056.svg"
+  },
+  {
+    "revision": "783e21a7b93352b650d0ac95bb7109b2",
+    "url": "./static/media/setting.783e21a7.svg"
+  },
+  {
+    "revision": "928fc4cf4497553c15dd90a78c59670d",
+    "url": "./static/media/signTopicMap.928fc4cf.png"
+  },
+  {
+    "revision": "6387bb88680f97f011bed8f50202defd",
+    "url": "./static/media/signViceMap.6387bb88.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "./static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "./static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "./static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "./static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "85296a6e51c538bdd721aea60ad31e30",
+    "url": "./static/media/triangle.85296a6e.svg"
+  },
+  {
+    "revision": "9688c417fe87caf816abc72272853b66",
+    "url": "./static/media/under-arrow.9688c417.svg"
+  },
+  {
+    "revision": "049214462e3608f66a408ee2c1e239eb",
+    "url": "./static/media/wechat-hover.04921446.svg"
+  },
+  {
+    "revision": "39ecdbccfb49534109e9a7f60e4fbb60",
+    "url": "./static/media/wechat.39ecdbcc.svg"
+  },
+  {
+    "revision": "21166a383165b8a9faa231bf391802bb",
+    "url": "./static/media/welcome.21166a38.png"
+  },
+  {
+    "revision": "244fa3d6b11f1c37bfda712e41a6a656",
+    "url": "./static/media/已签到.244fa3d6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f560a536baef5586b309d03ac51d0703.js"
+  "./precache-manifest.3a32adbeb4173db648dc6d26cc876746.js"
 );
 
 self.addEventListener('message', (event) => {
