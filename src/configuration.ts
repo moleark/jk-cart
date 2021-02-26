0dc17daa701b1ee309d15d935029f19373b2f9db
ligsh <system6@jkchemical.com>
2021-02-26 10:11:27 +0800

    ligsh, ver1.1.139,升级tonva到1.9.17,修复点击注册账号无响应的问题

--- src/configuration.ts
+++ src/configuration.ts
@@ -7,7 +7,7 @@ export { CApp } from './CApp';
 
 export const appConfig: AppConfig = {
     appName: '百灵威系统工程部/cart',
-    version: '1.1.138',
+    version: '1.1.139',
     tvs: tvs,
     loginTop: jnkTop,
     oem: '百灵威',
