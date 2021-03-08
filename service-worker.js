8c61e3b5abf7f01058a0b1cbd86ced56ce120662
ZWGKing <nage@163.com>
2021-03-08 15:55:47 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0665e2118ec364403134764685aef2f9.js"
+  "./precache-manifest.147ed4b6cd802b6fa53efca339ffed1c.js"
 );
 
 self.addEventListener('message', (event) => {
