9d6c38f67cea81b2881661a84f7527bc72bdd18c
ligsh <system6@jkchemical.com>
2021-03-11 10:37:58 +0800

    Updates

---
+++ precache-manifest.c7651a89497fcadbd68aa526f03ed158.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc605eeb36f4f20561295b13efa4c36d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0b3e60b0126aac7545d2",
+    "url": "./static/css/2.e226fe3a.chunk.css"
+  },
+  {
+    "revision": "4241897940fb4ea09dd1",
+    "url": "./static/css/main.4fe4a8fa.chunk.css"
+  },
+  {
+    "revision": "0b3e60b0126aac7545d2",
+    "url": "./static/js/2.7ccec44b.chunk.js"
+  },
+  {
+    "revision": "e110fca35ae4c92a61ad3c6cb3e27085",
+    "url": "./static/js/2.7ccec44b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4241897940fb4ea09dd1",
+    "url": "./static/js/main.2525a6bc.chunk.js"
+  },
+  {
+    "revision": "f3bf36e81ba3e4da0e32",
+    "url": "./static/js/runtime-main.605d384a.js"
+  },
+  {
+    "revision": "79b8b498f3747692dc1bc8b30eba2ba8",
+    "url": "./static/media/AnalyticalChemistry.79b8b498.jpg"
+  },
+  {
+    "revision": "f85ff3e0a81d38e50a00c59f0687c339",
+    "url": "./static/media/AnalyticalChemistry.f85ff3e0.png"
+  },
+  {
+    "revision": "06f15feb8324ae70938e87c9b09032f6",
+    "url": "./static/media/Homemadelogo.06f15feb.png"
+  },
+  {
+    "revision": "d2ca7c1d32478dca1233f71951c67da2",
+    "url": "./static/media/J&K triangle combinations 5.d2ca7c1d.png"
+  },
+  {
+    "revision": "51e831d105a424e0052c7f96f7984d8e",
+    "url": "./static/media/J&K triangle combinations4.51e831d1.png"
+  },
+  {
+    "revision": "01dbd0da348ff1d648fef83113f945b1",
+    "url": "./static/media/LabSupplies.01dbd0da.jpg"
+  },
+  {
+    "revision": "3f6ce00f7bed58ef38b348940f7810ea",
+    "url": "./static/media/LabSupplies.3f6ce00f.png"
+  },
+  {
+    "revision": "5c2e6ff5bf4fefbcca588b64884c53e3",
+    "url": "./static/media/LifeScience.5c2e6ff5.png"
+  },
+  {
+    "revision": "d11aa936ae83c1f1499271279b7e69af",
+    "url": "./static/media/LifeScience.d11aa936.jpg"
+  },
+  {
+    "revision": "efdb439003a913ca538f26d5270fcc69",
+    "url": "./static/media/MaterialScience.efdb4390.jpg"
+  },
+  {
+    "revision": "f2439aa0014b9b2ac73689d5034c9934",
+    "url": "./static/media/MaterialScience.f2439aa0.png"
+  },
+  {
+    "revision": "2e58a251fa3d28ceb37ed09c95c1dd5f",
+    "url": "./static/media/Menu.2e58a251.svg"
+  },
+  {
+    "revision": "11e84ba2e7c08c7f20f97066b45b48d8",
+    "url": "./static/media/OrganicChemistry.11e84ba2.jpg"
+  },
+  {
+    "revision": "6d56971c2c7ae9eb3b49ec6630da17b8",
+    "url": "./static/media/OrganicChemistry.6d56971c.png"
+  },
+  {
+    "revision": "abe107d7081b4199afcab43a00f67a4d",
+    "url": "./static/media/TriangleShadingO.abe107d7.png"
+  },
+  {
+    "revision": "7a55757eea6c2a794ae9e83f9aaca29d",
+    "url": "./static/media/TriangleShadingT.7a55757e.png"
+  },
+  {
+    "revision": "c130720600192b97f2582c6d2e54ffbe",
+    "url": "./static/media/cards.c1307206.png"
+  },
+  {
+    "revision": "00019799eadee6abadc8f54734e78fd9",
+    "url": "./static/media/chevron-right-hover.00019799.svg"
+  },
+  {
+    "revision": "5643de9e51735bda32b52fe610c5a4db",
+    "url": "./static/media/commerce-and-shopping-hover.5643de9e.svg"
+  },
+  {
+    "revision": "fb7260439e550df7a3035930e783be66",
+    "url": "./static/media/commerce-and-shopping.fb726043.svg"
+  },
+  {
+    "revision": "3c4ef45ab083a69a59b74c4b6e8a5d2b",
+    "url": "./static/media/flags.3c4ef45a.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c2c9edc0d1b3923dc925e7404280ba82",
+    "url": "./static/media/footer-bg.c2c9edc0.svg"
+  },
+  {
+    "revision": "015bdb05607e99a055a2ee8d342a3d17",
+    "url": "./static/media/giftPlate.015bdb05.png"
+  },
+  {
+    "revision": "89cdf192cc2a11fbe8e040d00be8eb0a",
+    "url": "./static/media/homeTopicMap.89cdf192.jpg"
+  },
+  {
+    "revision": "ce9e0db2426657f1e934e85f59aec03f",
+    "url": "./static/media/infb.ce9e0db2.jpg"
+  },
+  {
+    "revision": "eb360cc28d0f3686b967dcaf2b9d9a4d",
+    "url": "./static/media/left-arrow.eb360cc2.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "644af5a600da30d4813730f9f3249e86",
+    "url": "./static/media/logo_pointShop.644af5a6.png"
+  },
+  {
+    "revision": "5dfc14dd12e6652dc1a291f313cd0fc7",
+    "url": "./static/media/name-reation-bg-left.5dfc14dd.svg"
+  },
+  {
+    "revision": "402a544233824c8adb614ccd7c828ef9",
+    "url": "./static/media/name-reation-bg-right.402a5442.svg"
+  },
+  {
+    "revision": "772a31a646cb82c2cf0bdebade5fee51",
+    "url": "./static/media/pig.772a31a6.png"
+  },
+  {
+    "revision": "71e0aaee15a9d4ad29846871a47d7bde",
+    "url": "./static/media/s-triangle.71e0aaee.svg"
+  },
+  {
+    "revision": "928fc4cf4497553c15dd90a78c59670d",
+    "url": "./static/media/signTopicMap.928fc4cf.png"
+  },
+  {
+    "revision": "6387bb88680f97f011bed8f50202defd",
+    "url": "./static/media/signViceMap.6387bb88.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "./static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "./static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "./static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "./static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "cd60def72bf9d80fb022b37bac886aa5",
+    "url": "./static/media/under-arrow-1hover.cd60def7.svg"
+  },
+  {
+    "revision": "58976ad5a825e65a670c820b03b42b01",
+    "url": "./static/media/under-arrow-hover.58976ad5.svg"
+  },
+  {
+    "revision": "3b9f39c29162e0d2689612f27c0b2176",
+    "url": "./static/media/under-arrow.3b9f39c2.svg"
+  },
+  {
+    "revision": "21166a383165b8a9faa231bf391802bb",
+    "url": "./static/media/welcome.21166a38.png"
+  },
+  {
+    "revision": "244fa3d6b11f1c37bfda712e41a6a656",
+    "url": "./static/media/已签到.244fa3d6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6dc2b7c1e0e015ab3c40e53fc46de9ca.js"
+  "./precache-manifest.c7651a89497fcadbd68aa526f03ed158.js"
 );
 
 self.addEventListener('message', (event) => {
