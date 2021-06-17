cf8eb050ca4ae41a8450ec5180e4cfe0f82833a3
ZWGKing <nage@163.com>
2021-06-17 10:06:38 +0800

    zwg,U;1.修复发票填写携带空格报错;

--- src/customer/VInvoiceInfo.tsx
+++ src/customer/VInvoiceInfo.tsx
@@ -32,6 +32,7 @@ const uiSchema: UiSchema = {
             widget: 'text', label: '纳税人识别码', placeholder: '必填',
             rules: (value: string) => {
                 if (value) {
+                    value = value.trim();
                     var regArr = [/^[\da-z]{10,15}$/i, /^\d{6}[\da-z]{10,12}$/i, /^[a-z]\d{6}[\da-z]{9,11}$/i, /^[a-z]{2}\d{6}[\da-z]{8,10}$/i, /^\d{14}[\dx][\da-z]{4,5}$/i, /^\d{17}[\dx][\da-z]{1,2}$/i, /^[a-z]\d{14}[\dx][\da-z]{3,4}$/i, /^[a-z]\d{17}[\dx][\da-z]{0,1}$/i, /^[\d]{6}[\da-z]{13,14}$/i],
                         j = regArr.length;
                     for (var i = 0; i < j; i++) {
@@ -52,6 +53,7 @@ const uiSchema: UiSchema = {
         telephone: {
             widget: 'text', label: '注册电话', placeholder: '必填',
             rules: (value: string) => {
+                value = value.trim();
                 if (value && !/^(\(\d{3,4}\)|\d{3,4}-|\s)?\d{7,14}$/.test(value))
                     return "注册电话格式不正确，请重新输入！";
                 else
@@ -67,6 +69,7 @@ const uiSchema: UiSchema = {
         accountNo: {
             widget: 'text', label: '银行账号', placeholder: '必填',
             rules: (value: string) => {
+                value = value.trim();
                 // if (value && !/^([1-9]{1})(\d{14}|\d{17}|\d{18}|\d{15})$/.test(value))
                 /* 银行卡位数校验 现已开放 11-30位,后期可针对具体银行进行细化校验 */
                 /* 二次修改  客户银行卡号存在 0开头,暂时只校验数字及数位 */
