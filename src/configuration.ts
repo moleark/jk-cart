5d7c99f80b1bb633a197fdac173e206abaa70235
ZWGKing <nage@163.com>
2021-07-06 16:50:53 +0800

    zwg,U;1.ver1.1.141;

--- src/configuration.ts
+++ src/configuration.ts
@@ -7,7 +7,7 @@ export { CApp } from './CApp';
 
 export const appConfig: AppConfig = {
     appName: '百灵威系统工程部/cart',
-    version: '1.1.140',
+    version: '1.1.141',
     tvs: tvs,
     loginTop: jnkTop,
     oem: '百灵威',
