c41a8859da855ebf530bd089ee81ae103321dc21
ligsh <system6@jkchemical.com>
2021-07-18 21:56:26 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e916ec9bbb05d52e8d02e8aec8a86a4b.js"
+  "/precache-manifest.70d2f5a3eb5386b8d5854b54e758b11c.js"
 );
 
 self.addEventListener('message', (event) => {
